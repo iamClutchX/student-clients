ac189dedd98299530c2419ada24909cb139007ca
iamClutchX <iamr.karan@gmail.com>
2023-04-07 19:48:39 +0530

    add update delete api to app

--- src/api.js
+++ src/api.js
@@ -22,3 +22,28 @@ export const getStudents = async () => {
     throw new Error('An error occurred while getting the students');
   }
 };
+
+export const updateStudent = async (student) => {
+  try {
+    const response = await axios.put(
+      `${serverUrl}/api/students/${student._id}`,
+      student
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error('An error occurred while updating the student');
+  }
+};
+
+export const deleteStudent = async (studentId) => {
+  try {
+    const response = await axios.delete(`${serverUrl}/api/students/${studentId}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error('An error occurred while deleting the student');
+  }
+};
+
+
